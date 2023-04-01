a0c462aab545c5dcec3ea1fe8464bfc7966fede7
bannovdaniil <baddan@mail.ru>
2023-04-01 21:31:51 +0300

    !6: refactoring обновил schema.sql и schema.png поменял зависимость. Segments <-> Questions на ManyToMany

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,7 +3,19 @@ DROP TABLE IF EXISTS questions CASCADE;
 DROP TABLE IF EXISTS branches CASCADE;
 DROP TABLE IF EXISTS segments CASCADE;
 DROP TABLE IF EXISTS segment_owner CASCADE;
+DROP TABLE IF EXISTS segments_questions CASCADE;
 DROP TABLE IF EXISTS history CASCADE;
+
+CREATE TABLE IF NOT EXISTS branches
+(
+    id               BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    parent_branch_id BIGINT REFERENCES branches (id),
+    name             VARCHAR(128),
+    description      TEXT,
+    stable           BOOLEAN,
+    deleted          BOOLEAN,
+    created_time     TIMESTAMP
+);
 
 CREATE TABLE IF NOT EXISTS segment_owner
 (
@@ -19,22 +31,10 @@ CREATE TABLE IF NOT EXISTS segments
     name              VARCHAR(128),
     description       TEXT,
     segment_owner_id  BIGINT REFERENCES segment_owner (id),
+    branch_id         BIGINT REFERENCES branches (id),
     parent_segment_id BIGINT REFERENCES segments (id),
     role_id           BIGINT,
     key_value         VARCHAR(128)
-);
-
-
-CREATE TABLE IF NOT EXISTS branches
-(
-    id               BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    parent_branch_id BIGINT REFERENCES branches (id),
-    segment_id       BIGINT REFERENCES segments (id),
-    name             VARCHAR(128),
-    description      TEXT,
-    stable           BOOLEAN,
-    deleted          BOOLEAN,
-    created_time     TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS questions
@@ -42,7 +42,6 @@ CREATE TABLE IF NOT EXISTS questions
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name        VARCHAR(128),
     description TEXT,
-    branch_id   BIGINT REFERENCES branches (id),
     type_answer VARCHAR(15),
     active      BOOLEAN,
     required    BOOLEAN
@@ -56,6 +55,14 @@ CREATE TABLE IF NOT EXISTS answers
     question_id      BIGINT REFERENCES questions (id),
     question_next_id BIGINT REFERENCES questions (id),
     checked          BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS segments_questions
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    segment_id  BIGINT REFERENCES segments (id),
+    question_id BIGINT REFERENCES questions (id),
+    active      BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS history
