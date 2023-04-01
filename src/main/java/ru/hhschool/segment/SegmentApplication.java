e6950b6ebe3c7139aa0726373ce05a9ad553e70e
rubikkubika <98896050+rubikkubika@users.noreply.github.com>
2023-04-01 14:52:17 +0300

    downgrade springboot version to 2.7

--- src/main/java/ru/hhschool/segment/SegmentApplication.java
+++ src/main/java/ru/hhschool/segment/SegmentApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SegmentApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SegmentApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SegmentApplication.class, args);
+  }
 
 }
