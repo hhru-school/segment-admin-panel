6071861ee99241c260b8a5f39f3eb36ad3078688
bannovdaniil <baddan@mail.ru>
2023-04-02 18:03:10 +0300

    !8: refactoring обновил schema.sql и schema.png поменял зависимость, Переименование сущностей.. Commit ко всем таблицам на OneToMany-Many

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,46 +1,49 @@
 DROP TABLE IF EXISTS answers CASCADE;
 DROP TABLE IF EXISTS questions CASCADE;
-DROP TABLE IF EXISTS branches CASCADE;
+DROP TABLE IF EXISTS commits CASCADE;
 DROP TABLE IF EXISTS segments CASCADE;
-DROP TABLE IF EXISTS segment_owner CASCADE;
+DROP TABLE IF EXISTS segment_entrypoint CASCADE;
 DROP TABLE IF EXISTS segments_questions CASCADE;
 DROP TABLE IF EXISTS history CASCADE;
 
-CREATE TABLE IF NOT EXISTS branches
+
+CREATE TABLE IF NOT EXISTS commits
 (
     id               BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    parent_branch_id BIGINT REFERENCES branches (id),
-    name             VARCHAR(128),
+    parent_commit_id BIGINT REFERENCES commits (id),
+    name             VARCHAR(128) NOT NULL,
     description      TEXT,
     stable           BOOLEAN,
     deleted          BOOLEAN,
     created_time     TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS segment_owner
+CREATE TABLE IF NOT EXISTS segment_entrypoint
 (
-    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name        VARCHAR(128),
-    type_owner  VARCHAR(15),
-    description TEXT
+    id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    commit_id       BIGINT REFERENCES commits (id),
+    name            VARCHAR(128) NOT NULL,
+    description     TEXT,
+    type_entrypoint VARCHAR(15)
 );
 
 CREATE TABLE IF NOT EXISTS segments
 (
-    id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name              VARCHAR(128),
-    description       TEXT,
-    segment_owner_id  BIGINT REFERENCES segment_owner (id),
-    branch_id         BIGINT REFERENCES branches (id),
-    parent_segment_id BIGINT REFERENCES segments (id),
-    role_id           BIGINT,
-    key_value         VARCHAR(128)
+    id                    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    commit_id             BIGINT REFERENCES commits (id),
+    segment_entrypoint_id BIGINT REFERENCES segment_entrypoint (id),
+    parent_segment_id     BIGINT REFERENCES segments (id),
+    role_id               BIGINT,
+    key_value             VARCHAR(128),
+    name                  VARCHAR(128) NOT NULL,
+    description           TEXT
 );
 
 CREATE TABLE IF NOT EXISTS questions
 (
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name        VARCHAR(128),
+    commit_id   BIGINT REFERENCES commits (id),
+    name        VARCHAR(128) NOT NULL,
     description TEXT,
     type_answer VARCHAR(15),
     active      BOOLEAN,
@@ -50,28 +53,30 @@ CREATE TABLE IF NOT EXISTS questions
 CREATE TABLE IF NOT EXISTS answers
 (
     id               BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name             VARCHAR(128),
-    description      TEXT,
+    commit_id        BIGINT REFERENCES commits (id),
     question_id      BIGINT REFERENCES questions (id),
     question_next_id BIGINT REFERENCES questions (id),
+    name             VARCHAR(128),
+    description      TEXT,
     checked          BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS segments_questions
+CREATE TABLE IF NOT EXISTS question_activate_links
 (
-    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    branch_id   BIGINT REFERENCES branches (id),
-    segment_id  BIGINT REFERENCES branches (id),
-    question_id BIGINT REFERENCES questions (id),
-    active      BOOLEAN
+    id                    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    commit_id             BIGINT REFERENCES commits (id),
+    segment_id            BIGINT REFERENCES commits (id),
+    question_id           BIGINT REFERENCES questions (id),
+    segment_entrypoint_id BIGINT REFERENCES segment_entrypoint (id),
+    active                BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS history
 (
     id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    type        VARCHAR(10),
-    name_db     VARCHAR(20),
     user_id     BIGINT,
-    description TEXT,
-    time        TIMESTAMP
+    name_db     VARCHAR(20),
+    time        TIMESTAMP,
+    type        VARCHAR(10),
+    description TEXT
 );
