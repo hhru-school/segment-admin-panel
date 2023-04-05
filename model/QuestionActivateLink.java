c484955b6f215566d75d7672c4b187c36c417cf4
bannovdaniil <baddan@mail.ru>
2023-04-05 06:28:45 +0300

    !12 fix: model переделал с моделей на просто ID упростит взаимодействие.

--- model/QuestionActivateLink.java
+++ model/QuestionActivateLink.java
@@ -2,11 +2,9 @@ import java.util.List;
 
 public class QuestionActivateLink {
     private Long id;
-    private Commit commit;
-    private SegmentEntrypoint segmentEntrypoint;
-    private Segment segment;
-    private Question question;
-    private String positiveTitle;
-    private AnswerType answerType;
+    private Long commitId;
+    private Long segmentEntrypointId;
+    private Long segmentId;
+    private Long questionId;
     private boolean active;
 }
