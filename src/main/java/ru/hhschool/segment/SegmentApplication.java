089889f3581dfb1aa1aaaf84bcbd2f1b4c55284d
pigusov <pigusovaa@gmail.com>
2023-04-09 00:45:44 +0300

    Добавлены настройки подключения к PostgreSQL

--- src/main/java/ru/hhschool/segment/SegmentApplication.java
+++ src/main/java/ru/hhschool/segment/SegmentApplication.java
@@ -1,12 +1,22 @@
 package ru.hhschool.segment;
 
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SegmentApplication {
+  protected static SessionFactory sessionFactory;
   public static void main(String[] args) {
     SpringApplication.run(SegmentApplication.class, args);
+    sessionFactory = DbFactory.createSessionFactory();
+    try {
+      System.out.println(sessionFactory.getCurrentSession().getStatistics());;
+    } catch (HibernateException hex){
+      System.out.println(hex.getMessage());
+    }
   }
 
 }
