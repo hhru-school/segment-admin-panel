a13f6eafabfe15d31f932d9431c02fa6d660d6ed
bannovdaniil <baddan@mail.ru>
2023-04-14 22:07:11 +0300

    !4: добавил Dto

---
+++ src/main/java/ru/hhschool/segment/model/dto/AnswerChangeDto.java
@@ -0,0 +1,54 @@
+package ru.hhschool.segment.model.dto;
+
+import ru.hhschool.segment.model.enums.AnswerType;
+
+import java.util.List;
+
+public class AnswerChangeDto {
+    private Long id;
+    private String title;
+    private AnswerType answerType;
+    private List<QuestionChangeDto> openQuestionList;
+
+    public AnswerChangeDto() {
+    }
+
+    public AnswerChangeDto(Long id, String title, AnswerType answerType, List<QuestionChangeDto> openQuestionList) {
+        this.id = id;
+        this.title = title;
+        this.answerType = answerType;
+        this.openQuestionList = openQuestionList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public AnswerType getAnswerType() {
+        return answerType;
+    }
+
+    public void setAnswerType(AnswerType answerType) {
+        this.answerType = answerType;
+    }
+
+    public List<QuestionChangeDto> getOpenQuestionList() {
+        return openQuestionList;
+    }
+
+    public void setOpenQuestionList(List<QuestionChangeDto> openQuestionList) {
+        this.openQuestionList = openQuestionList;
+    }
+}
---
+++ src/main/java/ru/hhschool/segment/model/dto/QuestionChangeDto.java
@@ -0,0 +1,52 @@
+package ru.hhschool.segment.model.dto;
+
+import java.util.List;
+
+public class QuestionChangeDto {
+    private Long id;
+    private String title;
+    private String description;
+    private List<AnswerChangeDto> answerList;
+
+    public QuestionChangeDto() {
+    }
+
+    public QuestionChangeDto(Long id, String title, String description, List<AnswerChangeDto> answerList) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.answerList = answerList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<AnswerChangeDto> getAnswerList() {
+        return answerList;
+    }
+
+    public void setAnswerList(List<AnswerChangeDto> answerList) {
+        this.answerList = answerList;
+    }
+}
