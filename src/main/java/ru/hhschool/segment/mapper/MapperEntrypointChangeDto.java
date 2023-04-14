b5adf3bd2b065f5ad1462ecfec23f3bbd763f894
bannovdaniil <baddan@mail.ru>
2023-04-14 22:06:57 +0300

    !3: добавил мапперы

---
+++ src/main/java/ru/hhschool/segment/mapper/MapperEntrypointChangeDto.java
@@ -0,0 +1,25 @@
+package ru.hhschool.segment.mapper;
+
+import ru.hhschool.segment.model.dto.EntrypointChangeDto;
+import ru.hhschool.segment.model.entity.Entrypoint;
+
+import java.util.List;
+
+public class MapperEntrypointChangeDto {
+    public static EntrypointChangeDto entrypointChangeToDto(Entrypoint entrypoint) {
+        EntrypointChangeDto entrypointChangeDto = new EntrypointChangeDto(
+                entrypoint.getId(),
+                entrypoint.getTitle(),
+                entrypoint.getDescription()
+        );
+
+        return entrypointChangeDto;
+    }
+
+    public static List<EntrypointChangeDto> entrypointChangeListToDtoList(List<Entrypoint> entrypointList) {
+        return entrypointList
+                .stream()
+                .map(MapperEntrypointChangeDto::entrypointChangeToDto)
+                .toList();
+    }
+}
---
+++ src/main/java/ru/hhschool/segment/mapper/MapperQuestionChangeDto.java
@@ -0,0 +1,27 @@
+package ru.hhschool.segment.mapper;
+
+import ru.hhschool.segment.model.dto.QuestionChangeDto;
+import ru.hhschool.segment.model.entity.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapperQuestionChangeDto {
+    public static QuestionChangeDto questionChangeToDto(Question question) {
+        QuestionChangeDto questionChangeDto = new QuestionChangeDto(
+                question.getId(),
+                question.getTitle(),
+                question.getDescription(),
+                new ArrayList<>()
+        );
+
+        return questionChangeDto;
+    }
+
+    public static List<QuestionChangeDto> questionChangeListToDtoList(List<Question> questionList) {
+        return questionList
+                .stream()
+                .map(MapperQuestionChangeDto::questionChangeToDto)
+                .toList();
+    }
+}
