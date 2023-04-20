0f82e6be613c8f56bd496710df09ea8fa475954a
rubikkubika <98896050+rubikkubika@users.noreply.github.com>
2023-04-20 13:07:01 +0300

    Add question DAO

---
+++ src/main/java/ru/hhschool/segment/resource/QuestionResource.java
@@ -0,0 +1,28 @@
+package ru.hhschool.segment.resource;
+
+import ru.hhschool.segment.service.QuestionService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/questions")
+public class QuestionResource {
+  private final QuestionService questionService;
+
+  @Inject
+  public QuestionResource(QuestionService questionService) {
+    this.questionService = questionService;
+  }
+
+  @GET
+  @Path(value = "/{layerId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getQuestionDtoListWithAnswers(@PathParam("layerId") Long id) {
+    return Response.ok().build();
+  }
+}
