04745a0e1bf58b69e027b2336b91cd06e8c13a3c
bannovdaniil <baddan@mail.ru>
2023-04-20 06:35:19 +0300

    !6: рефакторинг: улучшил читаемость

--- src/main/java/ru/hhschool/segment/mapper/entrypointinfo/EntrypointWitchQuestionStatusMapper.java
+++ src/main/java/ru/hhschool/segment/mapper/entrypointinfo/EntrypointWitchQuestionStatusMapper.java
@@ -1,22 +1,24 @@
 package ru.hhschool.segment.mapper.entrypointinfo;
 
-import java.util.Collection;
 import ru.hhschool.segment.mapper.EntrypointMapper;
 import ru.hhschool.segment.model.dto.entrypointinfo.EntrypointWitchQuestionStatusDto;
 import ru.hhschool.segment.model.dto.entrypointinfo.QuestionStatusDto;
 import ru.hhschool.segment.model.entity.Entrypoint;
 
-public class EntrypointWitchQuestionStatusMapper {
-  public static EntrypointWitchQuestionStatusDto questionToQuestionStatusDto(
-      Entrypoint entrypoint,
-      Collection<QuestionStatusDto> questionStatusDtoSet
-  ) {
-    EntrypointWitchQuestionStatusDto entrypointWitchQuestionStatusDto =
-        new EntrypointWitchQuestionStatusDto(
-            EntrypointMapper.entrypointToDto(entrypoint),
-            questionStatusDtoSet
-        );
+import java.util.Set;
 
-    return entrypointWitchQuestionStatusDto;
-  }
+public class EntrypointWitchQuestionStatusMapper {
+    public static EntrypointWitchQuestionStatusDto questionToQuestionStatusDto(
+            Entrypoint entrypoint,
+            Set<QuestionStatusDto> questionStatusDtoSet
+    ) {
+
+        EntrypointWitchQuestionStatusDto entrypointWitchQuestionStatusDto =
+                new EntrypointWitchQuestionStatusDto(
+                        EntrypointMapper.entrypointToDto(entrypoint),
+                        questionStatusDtoSet
+                );
+
+        return entrypointWitchQuestionStatusDto;
+    }
 }
--- src/main/java/ru/hhschool/segment/model/dto/entrypointinfo/EntrypointWitchQuestionStatusDto.java
+++ src/main/java/ru/hhschool/segment/model/dto/entrypointinfo/EntrypointWitchQuestionStatusDto.java
@@ -1,35 +1,34 @@
 package ru.hhschool.segment.model.dto.entrypointinfo;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import ru.hhschool.segment.model.dto.EntrypointDto;
 
+import java.util.Set;
+
 public class EntrypointWitchQuestionStatusDto {
-  EntrypointDto entrypointDto;
-  Set<QuestionStatusDto> questionStatusDtoList;
+    EntrypointDto entrypointDto;
+    Set<QuestionStatusDto> questionStatusDtoList;
 
-  public EntrypointWitchQuestionStatusDto() {
-  }
+    public EntrypointWitchQuestionStatusDto() {
+    }
 
-  public EntrypointWitchQuestionStatusDto(EntrypointDto entrypointDto, Collection<QuestionStatusDto> questionStatusDtoList) {
-    this.entrypointDto = entrypointDto;
-    this.questionStatusDtoList = new HashSet<>(questionStatusDtoList);
-  }
+    public EntrypointWitchQuestionStatusDto(EntrypointDto entrypointDto, Set<QuestionStatusDto> questionStatusDtoSet) {
+        this.entrypointDto = entrypointDto;
+        this.questionStatusDtoList = questionStatusDtoSet;
+    }
 
-  public EntrypointDto getEntrypointDto() {
-    return entrypointDto;
-  }
+    public EntrypointDto getEntrypointDto() {
+        return entrypointDto;
+    }
 
-  public void setEntrypointDto(EntrypointDto entrypointDto) {
-    this.entrypointDto = entrypointDto;
-  }
+    public void setEntrypointDto(EntrypointDto entrypointDto) {
+        this.entrypointDto = entrypointDto;
+    }
 
-  public Set<QuestionStatusDto> getQuestionStatusDtoList() {
-    return questionStatusDtoList;
-  }
+    public Set<QuestionStatusDto> getQuestionStatusDtoList() {
+        return questionStatusDtoList;
+    }
 
-  public void setQuestionStatusDtoList(Set<QuestionStatusDto> questionStatusDtoList) {
-    this.questionStatusDtoList = questionStatusDtoList;
-  }
+    public void setQuestionStatusDtoList(Set<QuestionStatusDto> questionStatusDtoList) {
+        this.questionStatusDtoList = questionStatusDtoList;
+    }
 }
