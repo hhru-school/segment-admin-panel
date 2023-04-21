f328eb55ffbea5710609d2260cf2d58152fd494c
rubikkubika <98896050+rubikkubika@users.noreply.github.com>
2023-04-21 21:44:54 +0300

    add AnswerDao and AnswerMapper

--- src/main/java/ru/hhschool/segment/configuration/SegmentConfig.java
+++ src/main/java/ru/hhschool/segment/configuration/SegmentConfig.java
@@ -2,14 +2,17 @@ package ru.hhschool.segment.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.hhschool.segment.dao.abstracts.AnswerDao;
 import ru.hhschool.segment.dao.abstracts.EntrypointDao;
 import ru.hhschool.segment.dao.abstracts.LayerDao;
-import ru.hhschool.segment.dao.abstracts.QuestionDao;
 import ru.hhschool.segment.dao.abstracts.QuestionActivatorLinkDao;
+import ru.hhschool.segment.dao.abstracts.QuestionDao;
+import ru.hhschool.segment.dao.impl.AnswerDaoImpl;
 import ru.hhschool.segment.dao.impl.EntrypointDaoImpl;
 import ru.hhschool.segment.dao.impl.LayerDaoImpl;
-import ru.hhschool.segment.dao.impl.QuestionDaoImpl;
 import ru.hhschool.segment.dao.impl.QuestionActivatorLinkDaoImpl;
+import ru.hhschool.segment.dao.impl.QuestionDaoImpl;
+import ru.hhschool.segment.service.AnswerService;
 import ru.hhschool.segment.service.EntrypointService;
 import ru.hhschool.segment.service.LayerService;
 import ru.hhschool.segment.service.QuestionService;
@@ -25,14 +28,17 @@ public class SegmentConfig {
   public LayerDao getLayerDao() {
     return new LayerDaoImpl();
   }
+
   @Bean
   public QuestionDao getQuestionDao() {
     return new QuestionDaoImpl();
   }
+
   @Bean
-  public QuestionService getQuestionService(LayerDao layerDao,QuestionDao questionDao, QuestionActivatorLinkDao questionActivatorLinkDao) {
+  public QuestionService getQuestionService(LayerDao layerDao, QuestionDao questionDao, QuestionActivatorLinkDao questionActivatorLinkDao) {
     return new QuestionService(layerDao, questionDao, questionActivatorLinkDao);
   }
+
   @Bean
   public EntrypointDao getEntrypointDao() {
     return new EntrypointDaoImpl();
@@ -46,5 +52,15 @@ public class SegmentConfig {
   @Bean
   public QuestionActivatorLinkDao getQuestionActivatorLinkDao() {
     return new QuestionActivatorLinkDaoImpl();
+  }
+
+  @Bean
+  public AnswerDao getAnswerDao() {
+    return new AnswerDaoImpl();
+  }
+
+  @Bean
+  public AnswerService getAnswerService(AnswerDao answerDao) {
+    return new AnswerService(answerDao);
   }
 }
--- src/main/java/ru/hhschool/segment/service/AnswerService.java
+++ src/main/java/ru/hhschool/segment/service/AnswerService.java
@@ -1,4 +1,14 @@
 package ru.hhschool.segment.service;
 
+import ru.hhschool.segment.dao.abstracts.AnswerDao;
+
+import javax.inject.Inject;
+
 public class AnswerService {
+  private final AnswerDao answerDao;
+
+  @Inject
+  public AnswerService(AnswerDao answerDao) {
+    this.answerDao = answerDao;
+  }
 }
