763c6744b0925ddd586c7ecd84009224d8096854
bannovdaniil <baddan@mail.ru>
2023-04-23 22:07:28 +0300

    !4: поиск конфликтов в Вопросах, Ответах, Сегментах еще TODO

--- src/main/java/ru/hhschool/segment/util/LayerConflictChecker.java
+++ src/main/java/ru/hhschool/segment/util/LayerConflictChecker.java
@@ -6,10 +6,16 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import ru.hhschool.segment.mapper.change.MapperAnswerChange;
 import ru.hhschool.segment.mapper.change.MapperEntrypointChange;
 import ru.hhschool.segment.mapper.change.MapperLayerChange;
+import ru.hhschool.segment.mapper.change.MapperQuestionChange;
+import ru.hhschool.segment.mapper.change.MapperSegmentChange;
+import ru.hhschool.segment.model.dto.change.AnswerChangeDto;
 import ru.hhschool.segment.model.dto.change.EntrypointChangeDto;
 import ru.hhschool.segment.model.dto.change.LayerChangeDto;
+import ru.hhschool.segment.model.dto.change.QuestionChangeDto;
+import ru.hhschool.segment.model.dto.change.SegmentChangeDto;
 import ru.hhschool.segment.model.entity.Entrypoint;
 import ru.hhschool.segment.model.entity.Layer;
 import ru.hhschool.segment.model.enums.ConflictStatus;
@@ -21,19 +27,64 @@ public class LayerConflictChecker {
 
     boolean conflict = ConflictStatus.NONE.isConflict();
 
-    List<EntrypointChangeDto> entrypointConflictList =
-        new EntityConflictChecker<EntrypointChangeDto>().getEntityConflict(
-            MapperEntrypointChange.entrypointChangeListToDtoList(layer.getEntrypointList()),
-            MapperEntrypointChange.entrypointChangeListToDtoList(layerStableChild.getEntrypointList())
-
-        );
+    List<EntrypointChangeDto> entrypointConflictList = getEntrypointConflictList(layer, layerStableChild);
     if (entrypointConflictList.size() != 0) {
-      conflict ^= ConflictStatus.CONFLICT.isConflict();
+      conflict = ConflictStatus.CONFLICT.isConflict();
       layerChangeDto.setEntrypointMap(Map.of(EntityStatus.CONFLICT.name(), entrypointConflictList));
     }
 
+    List<QuestionChangeDto> questionConflictList = getQuestionConflictList(layer, layerStableChild);
+    if (questionConflictList.size() != 0) {
+      conflict = ConflictStatus.CONFLICT.isConflict();
+      layerChangeDto.setQuestionMap(Map.of(EntityStatus.CONFLICT.name(), questionConflictList));
+    }
+
+    List<AnswerChangeDto> answerChangeList = getAnswerConflictList(layer, layerStableChild);
+    if (answerChangeList.size() != 0) {
+      conflict = ConflictStatus.CONFLICT.isConflict();
+      layerChangeDto.setAnswerMap(Map.of(EntityStatus.CONFLICT.name(), answerChangeList));
+    }
+
+    List<SegmentChangeDto> segmentChangeList = getSegmentConflictList(layer, layerStableChild);
+    if (segmentChangeList.size() != 0) {
+      //TODO тут только титлы, а у нас еще тут надо еще сравнить и ParentId
+      conflict = ConflictStatus.CONFLICT.isConflict();
+      layerChangeDto.setSegmentMap(Map.of(EntityStatus.CONFLICT.name(), segmentChangeList));
+    }
+
+    //TODO поиск конфликтов в Links
+
     layerChangeDto.setConflict(conflict);
     return Optional.of(layerChangeDto);
+  }
+
+  private static List<SegmentChangeDto> getSegmentConflictList(Layer layer, Layer layerStableChild) {
+    return new EntityConflictChecker<SegmentChangeDto>().getEntityConflict(
+        MapperSegmentChange.segmentChangeListToDtoList(layer.getSegmentList()),
+        MapperSegmentChange.segmentChangeListToDtoList(layerStableChild.getSegmentList())
+    );
+  }
+
+  private static List<AnswerChangeDto> getAnswerConflictList(Layer layer, Layer layerStableChild) {
+    return new EntityConflictChecker<AnswerChangeDto>().getEntityConflict(
+        MapperAnswerChange.answerChangeListToDtoList(layer.getAnswerList()),
+        MapperAnswerChange.answerChangeListToDtoList(layerStableChild.getAnswerList())
+    );
+  }
+
+  private static List<QuestionChangeDto> getQuestionConflictList(Layer layer, Layer layerStableChild) {
+    return new EntityConflictChecker<QuestionChangeDto>().getEntityConflict(
+        MapperQuestionChange.questionChangeListToDtoList(layer.getQuestionList()),
+        MapperQuestionChange.questionChangeListToDtoList(layerStableChild.getQuestionList())
+    );
+  }
+
+  private static List<EntrypointChangeDto> getEntrypointConflictList(Layer layer, Layer layerStableChild) {
+    return new EntityConflictChecker<EntrypointChangeDto>().getEntityConflict(
+        MapperEntrypointChange.entrypointChangeListToDtoList(layer.getEntrypointList()),
+        MapperEntrypointChange.entrypointChangeListToDtoList(layerStableChild.getEntrypointList())
+
+    );
   }
 
   private static List<EntrypointChangeDto> isEntrypointConflict(List<Entrypoint> listA, List<Entrypoint> listB) {
