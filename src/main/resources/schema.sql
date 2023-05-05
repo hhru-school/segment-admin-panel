f08ee8e2e45c9401a1487aea414c479529dc9932
bannovdaniil <baddan@mail.ru>
2023-05-05 22:54:38 +0300

    schema v7

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS platforms CASCADE;
 DROP TABLE IF EXISTS applications CASCADE;
 DROP TABLE IF EXISTS layers CASCADE;
 DROP TABLE IF EXISTS entrypoints CASCADE;
@@ -8,14 +9,22 @@ DROP TABLE IF EXISTS screens CASCADE;
 DROP TABLE IF EXISTS segment_application_screen_links CASCADE;
 DROP TABLE IF EXISTS question_required_links CASCADE;
 DROP TABLE IF EXISTS screen_question_links CASCADE;
+DROP TABLE IF EXISTS application_platforms CASCADE;
 DROP TABLE IF EXISTS history CASCADE;
+
+CREATE TABLE IF NOT EXISTS platforms
+(
+    platform_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    platform    VARCHAR(255),
+    version     VARCHAR(255) NOT NULL
+);
+COMMENT ON COLUMN platforms.platform IS 'enum (ANDROID, IOS, WEB)';
+
 
 CREATE TABLE IF NOT EXISTS applications
 (
     application_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title          VARCHAR(255) NOT NULL,
-    platform       VARCHAR(255) NOT NULL,
-    version        VARCHAR(255) NOT NULL,
     description    VARCHAR(255) NOT NULL,
     state          VARCHAR(255)
 );
@@ -89,35 +98,45 @@ COMMENT ON COLUMN questions.answer_type IS 'enum (ACTIVE, ARCHIVE)';
 
 CREATE TABLE IF NOT EXISTS segment_application_screen_links
 (
-    id             BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    layer_id       BIGINT REFERENCES layers (layer_id),
-    application_id BIGINT REFERENCES applications (application_id),
-    segment_id     BIGINT REFERENCES segments (segment_id),
-    entrypoint_id  BIGINT REFERENCES entrypoints (entrypoint_id),
-    screen_id      BIGINT REFERENCES screens (screen_id),
-    screen_order   INT
+    id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    layer_id        BIGINT REFERENCES layers (layer_id),
+    segment_id      BIGINT REFERENCES segments (segment_id),
+    entrypoint_id   BIGINT REFERENCES entrypoints (entrypoint_id),
+    application_id  BIGINT REFERENCES applications (application_id),
+    screen_id       BIGINT REFERENCES screens (screen_id),
+    screen_position INT
 );
 
 CREATE TABLE IF NOT EXISTS question_required_links
 (
     id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     layer_id          BIGINT REFERENCES layers (layer_id),
-    application_id    BIGINT REFERENCES entrypoints (entrypoint_id),
     segment_id        BIGINT REFERENCES segments (segment_id),
+    application_id    BIGINT REFERENCES applications (application_id),
     question_id       BIGINT REFERENCES questions (question_id),
     question_required BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS screen_question_links
 (
-    id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    layer_id          BIGINT REFERENCES layers (layer_id),
-    segment_id        BIGINT REFERENCES segments (segment_id),
-    screen_id         BIGINT REFERENCES screens (screen_id),
-    question_id       BIGINT REFERENCES questions (question_id),
-    question_required BOOLEAN
+    id                  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    layer_id            BIGINT REFERENCES layers (layer_id),
+    segment_id          BIGINT REFERENCES segments (segment_id),
+    application_id      BIGINT REFERENCES applications (application_id),
+    screen_id           BIGINT REFERENCES screens (screen_id),
+    question_id         BIGINT REFERENCES questions (question_id),
+    question_position   INT,
+    question_visibility VARCHAR(255)
 );
-COMMENT ON COLUMN layers.state IS 'enum (SHOW,  HIDE,  HIDE_PREFILLED)';
+COMMENT ON COLUMN screen_question_links.question_visibility IS 'enum (SHOW,  HIDE,  HIDE_PREFILLED)';
+
+CREATE TABLE IF NOT EXISTS application_platforms
+(
+    id             BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    application_id BIGINT REFERENCES applications (application_id),
+    platform_id    BIGINT REFERENCES platforms (platform_id)
+);
+
 
 -- CREATE TABLE IF NOT EXISTS history
 -- (
