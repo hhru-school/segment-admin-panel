b083c54ab8636d9c849ab2744f2ce4f9e48ce82e
bannovdaniil <baddan@mail.ru>
2023-05-05 21:41:48 +0300

    !1 schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,25 @@
-DROP TABLE IF EXISTS answers CASCADE;
-DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS applications CASCADE;
 DROP TABLE IF EXISTS layers CASCADE;
-DROP TABLE IF EXISTS segments CASCADE;
 DROP TABLE IF EXISTS entrypoints CASCADE;
+DROP TABLE IF EXISTS segments CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS screens CASCADE;
+DROP TABLE IF EXISTS segment_application_screen_links CASCADE;
+DROP TABLE IF EXISTS question_required_links CASCADE;
+DROP TABLE IF EXISTS screen_question_links CASCADE;
 DROP TABLE IF EXISTS history CASCADE;
-DROP TABLE IF EXISTS question_activate_links CASCADE;
+
+CREATE TABLE IF NOT EXISTS applications
+(
+    application_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title          VARCHAR(255) NOT NULL,
+    platform       VARCHAR(255) NOT NULL,
+    version        VARCHAR(255) NOT NULL,
+    description    VARCHAR(255) NOT NULL,
+    state          VARCHAR(255)
+);
+COMMENT ON COLUMN applications.state IS 'enum (ARCHIVE, ACTIVE)';
 
 CREATE TABLE IF NOT EXISTS layers
 (
@@ -12,11 +27,10 @@ CREATE TABLE IF NOT EXISTS layers
     parent_layer_id BIGINT REFERENCES layers (layer_id),
     title           VARCHAR(255)             NOT NULL,
     description     VARCHAR(255),
-    layer_stable    BOOLEAN,
-    layer_archive   BOOLEAN,
-    layer_deleted   BOOLEAN,
+    state           VARCHAR(255),
     create_time     TIMESTAMP WITH TIME ZONE NOT NULL
 );
+COMMENT ON COLUMN layers.state IS 'enum (STABLE, ARCHIVE, TEST)';
 
 CREATE TABLE IF NOT EXISTS entrypoints
 (
@@ -35,51 +49,83 @@ CREATE TABLE IF NOT EXISTS segments
     description       VARCHAR(255),
     role              BIGINT[],
     tag               VARCHAR(255)[],
-    archived          BOOLEAN DEFAULT FALSE
+    state             VARCHAR(255)
 );
+COMMENT ON COLUMN segments.state IS 'enum (ARCHIVE, ACTIVE)';
 
 CREATE TABLE IF NOT EXISTS questions
 (
-    question_id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    layer_id            BIGINT REFERENCES layers (layer_id),
-    question_title      VARCHAR(255) NOT NULL,
-    question_type       VARCHAR(255) NOT NULL,
-    description         VARCHAR(255),
-    possible_answers    BIGINT[],
-    question_required   BOOLEAN,
-    question_visibility VARCHAR(255),
-    resume_field        BOOLEAN DEFAULT FALSE
+    question_id      BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title            VARCHAR(255) NOT NULL,
+    description      VARCHAR(255),
+    type             VARCHAR(255) NOT NULL,
+    answer_type      VARCHAR(255) NOT NULL,
+    possible_answers BIGINT[]
 );
+COMMENT ON COLUMN questions.type IS 'enum (RESUME_FIELD, QUESTION)';
+COMMENT ON COLUMN questions.answer_type IS 'enum (SINGLE_CHOICE, MULTI_SELECT,  NONE)';
 
 CREATE TABLE IF NOT EXISTS answers
 (
-    answer_id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    layer_id          BIGINT REFERENCES layers (layer_id),
-    open_questions    BIGINT[],
-    title             VARCHAR(255),
-    positive_title    VARCHAR(255),
-    answer_type       VARCHAR(255),
-    is_default_answer BOOLEAN NOT NULL,
-    skip_at_result    BOOLEAN
+    answer_id      BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    open_questions BIGINT[],
+    title          VARCHAR(255),
+    positive_title VARCHAR(255),
+    type           VARCHAR(255),
+    default_answer BOOLEAN NOT NULL,
+    skip_at_result BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS question_activate_links
+CREATE TABLE IF NOT EXISTS screens
 (
-    id                  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    layer_id            BIGINT REFERENCES layers (layer_id),
-    segment_id          BIGINT REFERENCES segments (segment_id),
-    question_id         BIGINT REFERENCES questions (question_id),
-    entrypoint_id       BIGINT REFERENCES entrypoints (entrypoint_id),
-    question_required   BOOLEAN,
-    question_visibility VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS history
-(
-    history_id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id     BIGINT,
-    name_db     VARCHAR(255),
-    time        TIMESTAMP WITH TIME ZONE NOT NULL,
+    screen_id   BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title       VARCHAR(255),
+    description VARCHAR(255),
     type        VARCHAR(255),
-    description TEXT
+    state       VARCHAR(255)
 );
+COMMENT ON COLUMN questions.type IS 'enum (STATIC, DYNAMIC)';
+COMMENT ON COLUMN questions.answer_type IS 'enum (ACTIVE, ARCHIVE)';
+
+CREATE TABLE IF NOT EXISTS segment_application_screen_links
+(
+    id             BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    layer_id       BIGINT REFERENCES layers (layer_id),
+    application_id BIGINT REFERENCES applications (application_id),
+    segment_id     BIGINT REFERENCES segments (segment_id),
+    entrypoint_id  BIGINT REFERENCES entrypoints (entrypoint_id),
+    screen_id      BIGINT REFERENCES screens (screen_id),
+    screen_order   INT
+);
+
+CREATE TABLE IF NOT EXISTS question_required_links
+(
+    id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    layer_id          BIGINT REFERENCES layers (layer_id),
+    application_id    BIGINT REFERENCES entrypoints (entrypoint_id),
+    segment_id        BIGINT REFERENCES segments (segment_id),
+    question_id       BIGINT REFERENCES questions (question_id),
+    question_required BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS screen_question_links
+(
+    id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    layer_id          BIGINT REFERENCES layers (layer_id),
+    segment_id        BIGINT REFERENCES segments (segment_id),
+    screen_id         BIGINT REFERENCES screens (screen_id),
+    question_id       BIGINT REFERENCES questions (question_id),
+    question_required BOOLEAN
+);
+COMMENT ON COLUMN layers.state IS 'enum (SHOW,  HIDE,  HIDE_PREFILLED)';
+
+-- CREATE TABLE IF NOT EXISTS history
+-- (
+--     history_id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     user_id     BIGINT,
+--     name_db     VARCHAR(255),
+--     time        TIMESTAMP WITH TIME ZONE NOT NULL,
+--     type        VARCHAR(255),
+--     description TEXT
+-- );
+-- COMMENT ON COLUMN history.type IS 'enum (CREATE, UPDATE, DELETE)';
