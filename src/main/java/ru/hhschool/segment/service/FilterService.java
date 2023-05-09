069cc37d4fc86633985c394de778b82103b209dc
Artem Retsko <artw123@mail.ru>
2023-05-09 09:29:28 +0300

    fix search issue

--- src/main/java/ru/hhschool/segment/service/FilterService.java
+++ src/main/java/ru/hhschool/segment/service/FilterService.java
@@ -1,5 +1,6 @@
 package ru.hhschool.segment.service;
 
+import com.sun.jdi.Value;
 import ru.hhschool.segment.model.dto.questioninfopage.AnswerDtoForQuestionsInfo;
 import ru.hhschool.segment.model.dto.questioninfopage.QuestionDtoForQuestionsInfo;
 
@@ -9,48 +10,49 @@ import java.util.List;
 import java.util.Map;
 
 public class FilterService {
-  Map<String, QuestionDtoForQuestionsInfo> filtredQuestionDtoMap = new HashMap<>();
+    Map<String, QuestionDtoForQuestionsInfo> filtredQuestionDtoMap = new HashMap<>();
 
-  public List<QuestionDtoForQuestionsInfo> filterQuestionDtoListByString(String searchString, List<QuestionDtoForQuestionsInfo> questionDtoList) {
-    filtredQuestionDtoMap.clear();
-    String searchStringLower = searchString.toLowerCase();
-    questionDtoList.forEach(questionDto -> {
-      checkQuestion(questionDto, searchStringLower);
-      checkAnswer(questionDto, searchStringLower);
-    });
-    return new ArrayList<>(filtredQuestionDtoMap.values());
-  }
-
-  public void checkQuestion(QuestionDtoForQuestionsInfo questionDto, String searchStringLower) {
-    String titleLower = questionDto.getTitle().toLowerCase();
-    String descriptionLower = questionDto.getDescription().toLowerCase();
-    if (titleLower.contains(searchStringLower) || descriptionLower.contains(searchStringLower)) {
-      questionDto.setSearchedObject(true);
-      filtredQuestionDtoMap.putIfAbsent(questionDto.getTitle(), questionDto);
+    public List<QuestionDtoForQuestionsInfo> filterQuestionDtoListByString(String searchString, List<QuestionDtoForQuestionsInfo> questionDtoList) {
+        filtredQuestionDtoMap.clear();
+        String searchStringLower = searchString.toLowerCase();
+        questionDtoList.forEach(questionDto -> {
+            checkQuestion(questionDto, searchStringLower);
+            checkAnswer(questionDto, searchStringLower);
+        });
+        List<QuestionDtoForQuestionsInfo> list = new ArrayList<QuestionDtoForQuestionsInfo>(filtredQuestionDtoMap.values());
+        return list;
     }
-  }
 
-  public void checkAnswer(QuestionDtoForQuestionsInfo questionDto, String searchStringLower) {
-    questionDto.getAnswerDtoList().forEach(answerDto -> {
-      String titleLower = answerDto.getTitle().toLowerCase();
-      if (titleLower.contains(searchStringLower)) {
-        answerDto.setSearchedObject(true);
-        filtredQuestionDtoMap.putIfAbsent(questionDto.getTitle(), questionDto);
-      }
-      checkOpenQuestion(answerDto, questionDto, searchStringLower);
-    });
-  }
+    public void checkQuestion(QuestionDtoForQuestionsInfo questionDto, String searchStringLower) {
+        String titleLower = questionDto.getTitle().toLowerCase();
+        String descriptionLower = questionDto.getDescription().toLowerCase();
+        if (titleLower.contains(searchStringLower) || descriptionLower.contains(searchStringLower)) {
+            questionDto.setSearchedObject(true);
+            filtredQuestionDtoMap.putIfAbsent(questionDto.getTitle(), questionDto);
+        }
+    }
 
-  public void checkOpenQuestion(AnswerDtoForQuestionsInfo answerDto, QuestionDtoForQuestionsInfo mainQuestionDto, String searchStringLower) {
-    answerDto.getOpenQuestonDtoList().forEach(questionDto -> {
-      String titleLower = questionDto.getTitle().toLowerCase();
-      String descriptionLower = questionDto.getDescription().toLowerCase();
-      if (titleLower.contains(searchStringLower) || descriptionLower.contains(searchStringLower)) {
-        questionDto.setSearchedObject(true);
-        filtredQuestionDtoMap.putIfAbsent(questionDto.getTitle(), mainQuestionDto);
-      }
-      checkAnswer(questionDto, searchStringLower);
-    });
-  }
+    public void checkAnswer(QuestionDtoForQuestionsInfo questionDto, String searchStringLower) {
+        questionDto.getAnswerDtoList().forEach(answerDto -> {
+            String titleLower = answerDto.getTitle().toLowerCase();
+            if (titleLower.contains(searchStringLower)) {
+                answerDto.setSearchedObject(true);
+                filtredQuestionDtoMap.putIfAbsent(questionDto.getTitle(), questionDto);
+            }
+            checkOpenQuestion(answerDto, questionDto, searchStringLower);
+        });
+    }
+
+    public void checkOpenQuestion(AnswerDtoForQuestionsInfo answerDto, QuestionDtoForQuestionsInfo mainQuestionDto, String searchStringLower) {
+        answerDto.getOpenQuestonDtoList().forEach(questionDto -> {
+            String titleLower = questionDto.getTitle().toLowerCase();
+            String descriptionLower = questionDto.getDescription().toLowerCase();
+            if (titleLower.contains(searchStringLower) || descriptionLower.contains(searchStringLower)) {
+                questionDto.setSearchedObject(true);
+                filtredQuestionDtoMap.putIfAbsent(mainQuestionDto.getTitle(), mainQuestionDto);
+            }
+            checkAnswer(questionDto, searchStringLower);
+        });
+    }
 }
 
