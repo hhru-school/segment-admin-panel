fc35a3b1baa485b4317ed95c20745b33dc7c5bf9
bannovdaniil <baddan@mail.ru>
2023-05-17 21:15:37 +0300

    2: fix: compile error

--- src/main/java/ru/hhschool/segment/util/LayerConflictChecker.java
+++ src/main/java/ru/hhschool/segment/util/LayerConflictChecker.java
@@ -1,15 +1,10 @@
 package ru.hhschool.segment.util;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import ru.hhschool.segment.mapper.change.LayerChangeMapper;
-import ru.hhschool.segment.mapper.change.SegmentChangeMapper;
 import ru.hhschool.segment.model.dto.change.LayerChangeDto;
-import ru.hhschool.segment.model.dto.change.SegmentChangeDto;
 import ru.hhschool.segment.model.entity.Layer;
 import ru.hhschool.segment.model.enums.ConflictStatus;
-import ru.hhschool.segment.model.enums.EntityStatus;
 
 public class LayerConflictChecker {
   public static Optional<LayerChangeDto> getConflict(Layer layer, Layer layerForCompare) {
@@ -21,7 +16,6 @@ public class LayerConflictChecker {
 
     conflict |= checkEntrypointConflict(layer, layerForCompare, layerChangeDto);
     conflict |= checkQuestionConflict(layer, layerForCompare, layerChangeDto);
-    conflict |= checkSegmentConflict(layer, layerForCompare, layerChangeDto);
     conflict |= checkQuestionActivatorLinkConflict(layer, layerForCompare, layerChangeDto);
 
     layerChangeDto.setConflict(conflict);
@@ -30,22 +24,6 @@ public class LayerConflictChecker {
 
   private static boolean checkQuestionActivatorLinkConflict(Layer layer, Layer layerForCompare, LayerChangeDto layerChangeDto) {
     boolean conflict = ConflictStatus.NONE.isConflict();
-    return conflict;
-  }
-
-  private static boolean checkSegmentConflict(Layer layer, Layer layerForCompare, LayerChangeDto layerChangeDto) {
-    boolean conflict = ConflictStatus.NONE.isConflict();
-    List<SegmentChangeDto> segmentChangeList
-        = new EntityConflictChecker<SegmentChangeDto>().getConflictList(
-        SegmentChangeMapper.segmentChangeListToDtoList(layer.getSegmentList()),
-        SegmentChangeMapper.segmentChangeListToDtoList(layerForCompare.getSegmentList())
-    );
-    if (segmentChangeList.size() != 0) {
-      conflict = ConflictStatus.CONFLICT.isConflict();
-      layerChangeDto.setSegmentMap(Map.of(EntityStatus.CONFLICT.name(), segmentChangeList));
-    } else {
-      layerChangeDto.setSegmentMap(Map.of());
-    }
     return conflict;
   }
 
