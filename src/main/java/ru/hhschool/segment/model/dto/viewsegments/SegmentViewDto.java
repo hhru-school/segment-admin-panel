e6a05b836143d4b7533f117f4b0a8b9f5977f815
pigusov <pigusovaa@gmail.com>
2023-05-26 15:35:57 +0300

    Рефакторинг

--- src/main/java/ru/hhschool/segment/model/dto/viewsegments/SegmentViewDto.java
+++ src/main/java/ru/hhschool/segment/model/dto/viewsegments/SegmentViewDto.java
@@ -1,5 +1,6 @@
 package ru.hhschool.segment.model.dto.viewsegments;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ru.hhschool.segment.model.dto.viewsegments.enums.SegmentViewChangeState;
 import ru.hhschool.segment.model.entity.Role;
 import ru.hhschool.segment.model.enums.StateType;
@@ -9,7 +10,9 @@ import java.util.List;
 public class SegmentViewDto {
   private Long id;
   private String title;
+  @JsonProperty("roleList")
   private List<Role> roles;
+  @JsonProperty("tagList")
   private List<String> tags;
   private SegmentViewChangeState changeState;
   private StateType activeState;
