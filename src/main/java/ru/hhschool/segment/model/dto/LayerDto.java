4984c816abaa23fc069d56cf2e4696c9db9f947d
pigusov <pigusovaa@gmail.com>
2023-05-26 17:03:43 +0300

    Фикс дубликатов тестовых данных

--- src/main/java/ru/hhschool/segment/model/dto/LayerDto.java
+++ src/main/java/ru/hhschool/segment/model/dto/LayerDto.java
@@ -6,7 +6,6 @@ import ru.hhschool.segment.model.enums.LayerStatus;
 public class LayerDto {
   private Long id;
   private String title;
-
   private LocalDateTime createTime;
   private LayerStatus layerStatus;
 
