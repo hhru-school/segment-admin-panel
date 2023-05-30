b6cecec7be2c2ed9b86a13d5ebe1bf72f8ba656a
rubikkubika <98896050+rubikkubika@users.noreply.github.com>
2023-05-30 10:39:35 +0300

    rework field page

--- src/main/java/ru/hhschool/segment/resource/QuestionResource.java
+++ src/main/java/ru/hhschool/segment/resource/QuestionResource.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -35,9 +36,9 @@ public class QuestionResource {
   }
 
   @GET
-  @Path(value = "/detail")
+  @Path(value = "/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response getQuestionDtoInfoWithAnswers(@NotNull @QueryParam("questionId") Long questionId) {
-    return Response.ok(questionService.getQuestionDtoForQuestionInfo(questionId)).build();
+  public Response getQuestionDtoInfoWithAnswers(@NotNull @PathParam("id") Long id) {
+    return Response.ok(questionService.getQuestionDtoForQuestionInfo(id)).build();
   }
 }
