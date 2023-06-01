eb9112463f0710c11a3f42ea66b308f686f89064
bannovdaniil <baddan@mail.ru>
2023-06-01 13:49:26 +0300

    !6 refactoring: убрал неиспользуемый метод. проблема в том что выборка происходит не в том порядке который в списке.

--- src/main/java/ru/hhschool/segment/dao/abstracts/QuestionDao.java
+++ src/main/java/ru/hhschool/segment/dao/abstracts/QuestionDao.java
@@ -1,8 +1,6 @@
 package ru.hhschool.segment.dao.abstracts;
 
-import java.util.List;
 import ru.hhschool.segment.model.entity.Question;
 
 public interface QuestionDao extends ReadWriteDao<Question, Long> {
-  List<Question> findAll(List<Long> questionsIdList);
 }
