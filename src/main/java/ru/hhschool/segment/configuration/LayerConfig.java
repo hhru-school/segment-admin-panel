d1659c61fbcd973c97bd9071778d4e488433cdda
pigusov <pigusovaa@gmail.com>
2023-06-04 02:30:01 +0300

    Добавил вывод платформ при просмотре информации о слое

--- src/main/java/ru/hhschool/segment/configuration/LayerConfig.java
+++ src/main/java/ru/hhschool/segment/configuration/LayerConfig.java
@@ -3,14 +3,15 @@ package ru.hhschool.segment.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.hhschool.segment.dao.abstracts.LayerDao;
+import ru.hhschool.segment.dao.abstracts.PlatformDao;
 import ru.hhschool.segment.dao.impl.LayerDaoImpl;
 import ru.hhschool.segment.service.LayerService;
 
 @Configuration
 public class LayerConfig {
   @Bean
-  public LayerService getLayerService(LayerDao layerDao) {
-    return new LayerService(layerDao);
+  public LayerService getLayerService(LayerDao layerDao, PlatformDao platformDao) {
+    return new LayerService(layerDao, platformDao);
   }
 
   @Bean
