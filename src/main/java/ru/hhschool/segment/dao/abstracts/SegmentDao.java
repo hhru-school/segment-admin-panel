e8d160a34d3c2bca452cac4a778adf7e100e2f63
bannovdaniil <baddan@mail.ru>
2023-06-04 21:02:17 +0300

    !3. fix: в Segment сущности нет теперь layerId, убрал метод.

--- src/main/java/ru/hhschool/segment/dao/abstracts/SegmentDao.java
+++ src/main/java/ru/hhschool/segment/dao/abstracts/SegmentDao.java
@@ -4,8 +4,5 @@ import java.util.List;
 import ru.hhschool.segment.model.entity.Segment;
 
 public interface SegmentDao extends ReadWriteDao<Segment, Long> {
-
-  List<Segment> findAll(Long layerId);
-
   List<Segment> findAll(String searchQuery);
 }
--- src/main/java/ru/hhschool/segment/dao/impl/SegmentDaoImpl.java
+++ src/main/java/ru/hhschool/segment/dao/impl/SegmentDaoImpl.java
@@ -6,13 +6,6 @@ import ru.hhschool.segment.model.entity.Segment;
 
 public class SegmentDaoImpl extends ReadWriteDaoImpl<Segment, Long> implements SegmentDao {
   @Override
-  public List<Segment> findAll(Long layerId) {
-    return em.createQuery("SELECT DISTINCT e FROM Segment e WHERE e.layerId = :layerId")
-        .setParameter("layerId", layerId)
-        .getResultList();
-  }
-
-  @Override
   public List<Segment> findAll(String searchQuery) {
     List<Segment> segmentList;
     if (searchQuery.isBlank()) {
