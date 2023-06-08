2d6c3cf5b96bbcb878ba5a5d9e895d705f252c42
pigusov <pigusovaa@gmail.com>
2023-06-09 02:59:34 +0300

    Добавил DTO для вывода информации о сегменте при создании слоя

---
+++ src/main/java/ru/hhschool/segment/model/dto/createlayer/CreateLayerRequirementDto.java
@@ -0,0 +1,52 @@
+package ru.hhschool.segment.model.dto.createlayer;
+
+
+public class CreateLayerRequirementDto {
+  private Long questionId;
+  private Long questionRequiredLinkId;
+  private String title;
+  private Boolean required;
+
+  public CreateLayerRequirementDto() {
+
+  }
+
+  public CreateLayerRequirementDto(Long questionId, Long questionRequiredLinkId, String title, Boolean required) {
+    this.questionId = questionId;
+    this.questionRequiredLinkId = questionRequiredLinkId;
+    this.title = title;
+    this.required = required;
+  }
+
+  public Long getQuestionId() {
+    return questionId;
+  }
+
+  public void setQuestionId(Long questionId) {
+    this.questionId = questionId;
+  }
+
+  public Long getQuestionRequiredLinkId() {
+    return questionRequiredLinkId;
+  }
+
+  public void setQuestionRequiredLinkId(Long questionRequiredLinkId) {
+    this.questionRequiredLinkId = questionRequiredLinkId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Boolean getRequired() {
+    return required;
+  }
+
+  public void setRequired(Boolean required) {
+    this.required = required;
+  }
+}
