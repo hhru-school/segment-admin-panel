dd734c052cf666fcd3fa7139fa4cc5299d59de89
rubikkubika <98896050+rubikkubika@users.noreply.github.com>
2023-06-20 11:38:04 +0300

    fix issues

--- src/main/java/ru/hhschool/segment/resource/LayerResource.java
+++ src/main/java/ru/hhschool/segment/resource/LayerResource.java
@@ -1,9 +1,13 @@
 package ru.hhschool.segment.resource;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.hhschool.segment.exception.HttpBadRequestException;
+import ru.hhschool.segment.model.dto.ErrorDto;
 import ru.hhschool.segment.model.dto.LayerDto;
 import ru.hhschool.segment.model.dto.basicinfo.LayerBasicInfoDto;
 import ru.hhschool.segment.model.dto.createlayer.info.InfoLayerSegmentDto;
 import ru.hhschool.segment.model.dto.layer.LayerDtoForList;
+import ru.hhschool.segment.model.dto.layer.create.LayerCreateDto;
 import ru.hhschool.segment.model.dto.merge.MergeResponseDto;
 import ru.hhschool.segment.model.dto.viewsegments.layerview.LayerSegmentsDto;
 import ru.hhschool.segment.model.dto.viewsegments.layerview.SegmentSelectedDto;
@@ -24,19 +28,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.web.bind.annotation.RequestBody;
-import ru.hhschool.segment.exception.HttpBadRequestException;
-import ru.hhschool.segment.model.dto.ErrorDto;
-import ru.hhschool.segment.model.dto.LayerDto;
-import ru.hhschool.segment.model.dto.basicinfo.LayerBasicInfoDto;
-import ru.hhschool.segment.model.dto.createlayer.info.InfoLayerSegmentDto;
-import ru.hhschool.segment.model.dto.layer.LayerDtoForList;
-import ru.hhschool.segment.model.dto.layer.create.LayerCreateDto;
-import ru.hhschool.segment.model.dto.viewsegments.layerview.LayerSegmentsDto;
-import ru.hhschool.segment.model.dto.viewsegments.layerview.SegmentSelectedDto;
-import ru.hhschool.segment.service.LayerService;
-import ru.hhschool.segment.service.SegmentService;
 
 
 @Path("/layers")
@@ -153,7 +144,7 @@ public class LayerResource {
   @Produces(MediaType.APPLICATION_JSON)
   public Response forceMergeLayer(@PathParam(value = "layerId") Long layerId) {
     MergeResponseDto mergeResponseDto = layerService.forceMergeLayer(layerId);
-    if (mergeResponseDto.getState().equals(LayerStateType.STABLE)){
+    if (mergeResponseDto.getState().equals(LayerStateType.STABLE)) {
       return Response.ok(mergeResponseDto).build();
     }
     return Response.status(Response.Status.CONFLICT).entity(mergeResponseDto).build();
